6fbf62db56108146e54466a4f311ec46bd6bfbf1
Mat Holroyd <matholroyd@gmail.com>
2009-04-21 13:20:02 +1000

    Added stuff to test

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,9 @@ class MembersController < ApplicationController
   make_resourceful do
     build :all
   end
+  
+  def test_worker
+    Job.enqueue!(ExampleWorker, :add, 1, 2)
+    redirect_to members_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :user_session, :except => [:edit, :show, :update]
-  map.resources :members
+  map.resources :members, :collection => {:test_worker => :get}
   map.resources :users
+  
+  map.namespace "admin" do |admin|
+    admin.resources :jobs
+  end
+  
+  map.root :controller => 'members', :action => 'index'
 end
