bd206764fd6d3ab96f2d53d8a82e81b585b0f1ea
Mat Holroyd <matholroyd@gmail.com>
2009-05-22 17:06:06 +1000

    Can add new members

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -2,8 +2,37 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe MembersController do
 
+  before :each do
+    @user = User.make
+    UserSession.create(@user)
+  end
+
+  describe 'responding to GET index' do
+    it 'should respond with success' do
+      get :index
+      response.should be_success
+    end
+  end
+  
+  describe 'responding to GET new' do
+    it 'should respond with success' do
+      get :index
+      response.should be_success
+    end
+  end
+
+  describe 'responding to POST create' do
+    it 'should respond with success' do
+      lambda {
+        post :create, :member => {:first_name => 'Bob'}
+      }.should change(Member, :count).by(1)
+    end
+  end
+
+
+
   describe 'responding to POST import' do
-    # post :import, 
+    
   end
   
 
