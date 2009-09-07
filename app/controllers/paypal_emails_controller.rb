22b68ac6af9fafb6d3bd91e8f52b6405b695f83e
Mat Holroyd <matholroyd@gmail.com>
2009-09-07 11:45:16 +1000

    Fixing the show action after upate

--- app/controllers/paypal_emails_controller.rb
+++ app/controllers/paypal_emails_controller.rb
@@ -4,9 +4,9 @@ class PaypalEmailsController < ApplicationController
   end
   
   def update
-    paypal_email = PaypalEmail.find(params[:id])
-    if paypal_email.update_attributes(params[:paypal_email])
-      member = paypal_email.member
+    @paypal_email = PaypalEmail.find(params[:id])
+    if @paypal_email.update_attributes(params[:paypal_email])
+      member = @paypal_email.member
       if member
         member.set_financial
         member.save!
@@ -19,17 +19,11 @@ class PaypalEmailsController < ApplicationController
   
   def show
     @paypal_email = PaypalEmail.find(params[:id])
-    if @paypal_email.receipt.nil?
-      @paypal_email.build_receipt
-      @paypal_email.receipt.receipt_number = 'paypal'
-      @paypal_email.receipt.amount = @paypal_email.guessed_amount_paid
-      @paypal_email.receipt.member_id = @paypal_email.guessed_member_id
-    end
   end
 
   def check_for_new
     PaypalEmail.import_pending
     redirect_to paypal_emails_path
   end
-  
+    
 end
